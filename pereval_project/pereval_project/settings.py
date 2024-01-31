31d017628a3663424d0d2d5d78f7111d0cebee29
daniel300319 <9xonner@gmail.com>
2024-01-31 22:16:33 +0300

    Обновлен settings

--- pereval_project/pereval_project/settings.py
+++ pereval_project/pereval_project/settings.py
@@ -124,6 +124,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
